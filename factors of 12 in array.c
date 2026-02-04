de122b73c4e201f4857df11c7d9ef69d35607620
Udaykiran218 <udaykiran2618@gmail.com>
2026-02-04 15:00:24 +0530

    Add files via upload

---
+++ factors of 12 in array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    int size;
+    printf("enter the array size:\n");
+    scanf("%d",&size);
+    int a[size];
+    for(int i=0;i<=size-1;i++)
+    {
+        printf("enter the %d elements \n",(i+1));
+        scanf("%d",&a[i]);
+    }
+    int sum=0;
+    for(int i=0;i<=size-1;i++)
+    {
+        sum=sum+a[i];
+    }
+   printf("the sum is %d ",sum);
+   for( int i=0;i<=size-1;i++)
+   {
+    if (12%a[i]==0)
+    {
+    printf("the %d is factor \n",a[i]);
+    }
+   }
+
+}
